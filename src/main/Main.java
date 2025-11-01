aa210e0d7199894e686baa1aac7c047801c113c0
DHMPROG <aminedahmanepro@gmail.com>
2025-11-01 14:59:20 -0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.JFrame;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+
+
+  }
+
